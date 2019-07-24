7c21afa6375ec78352c8e8625e9291026d6cea8c
lifang <12345678>
2019-07-24 17:48:38 +0800

    0724,spring boot

--- src/main/java/com/example/demo/pos/controller/UrlPathController.java
+++ src/main/java/com/example/demo/pos/controller/UrlPathController.java
@@ -1,7 +1,13 @@
 package com.example.demo.pos.controller;
 
+import com.example.demo.pos.dao.model.UrlPathModel;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * @RestController 注解的意思是，Controller里面的方法都以json的格式输出
@@ -15,5 +21,17 @@ public class UrlPathController {
         return "我是一个还未进化成真正list的list,嘿嘿嘿！";
     }
 
+
+    @RequestMapping("/saveUrl")
+    public void saveUser(@Valid UrlPathModel urlPathModel, BindingResult result){
+        System.out.println("保存");
+        if (result.hasErrors()){
+            List<ObjectError> allErrors = result.getAllErrors();
+            for (ObjectError error : allErrors) {
+                System.out.println(error.getCode()+ "-" + error.getDefaultMessage());
+            }
+        }
+
+    }
 
 }
